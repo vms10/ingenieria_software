00e8e02835a3686674f7b1d80ed63e23ac2b2fe6
vms10 <msolvidal10@hotmail.com>
2025-10-04 12:40:23 -0300

    cambios test api

---
+++ test_app.py
@@ -0,0 +1,24 @@
+import unittest
+from app import add, subtract, multiply
+
+
+class TestApp(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(1, 2), 3)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(5, 3), 2)
+        self.assertEqual(subtract(3, 5), -2)
+        self.assertEqual(subtract(10, 0), 10)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(2, 3), 6)
+        self.assertEqual(multiply(-1, 5), -5)
+        self.assertEqual(multiply(0, 10), 0)
+
+
+if __name__ == "__main__":
+    unittest.main()
